1cee233ce180c745c4c9267865b3d8b70eddbbc2
Chhitiz Buchasia <25711212+buchasia@users.noreply.github.com>
2020-12-12 17:32:45 +0100

    removed print statment

--- D05/D05Q2.py
+++ D05/D05Q2.py
@@ -15,7 +15,6 @@ def solveQuestion(inputPath):
         currentString = doorId + str(counter)
         hashString = hashlib.md5(currentString.encode()).hexdigest()
         if hashString[:5] == '00000' and hashString[5] != 0:
-            print(counter, hashString[5], hashString[6])
             if hashString[5] > '7':
                 continue
             if int(hashString[5]) not in password:

d5c4c7429b1838aa0a7bef6fa6bb0fcb36b91335
Chhitiz Buchasia <25711212+buchasia@users.noreply.github.com>
2020-12-18 23:21:07 +0100

    Finished Day 11

---
+++ D11/D11Q2.py
@@ -0,0 +1,50 @@
+def solveQuestion(inputPath):
+    
+    fileP = open(inputPath, 'r')
+    directions = fileP.readlines()[0].strip().split(',')
+    fileP.close()
+
+    currentX = 0
+    currentY = 0
+    directionSteps = {'n': [0, 2], 'ne': [1, 1], 's': [0, -2], 'se': [1, -1], 'nw': [-1, 1], 'sw': [-1, -1]}
+    maxSteps = -1
+    for direction in directions:
+        directionStep = directionSteps[direction]
+        currentX += directionStep[0]
+        currentY += directionStep[1]
+        currentSteps = getSteps(currentX, currentY)
+        if currentSteps > maxSteps:
+            maxSteps = currentSteps
+
+    return maxSteps
+
+def getSteps(currentX, currentY):
+    steps = 0
+    while currentX != 0 or currentY != 0:
+        if currentX > 0 and currentY >= 0:
+            steps += currentX
+            currentY -= currentX
+            currentX = 0
+        elif currentX > 0 and currentY <= 0:
+            steps += currentX
+            currentY += currentX
+            currentX = 0
+        elif currentX < 0 and currentY >= 0:
+            steps += abs(currentX)
+            currentY -= abs(currentX)
+            currentX = 0
+        elif currentX < 0 and currentY <= 0:
+            steps += abs(currentX)
+            currentY += abs(currentX)
+            currentX = 0
+        elif currentX == 0 and currentY > 0:
+            steps += currentY // 2
+            currentY = 0
+        elif currentX == 0 and currentY < 0:
+            steps += abs(currentY) // 2
+            currentY = 0
+        
+    return steps
+    
+    
+print(solveQuestion('InputD11Q2.txt'))

6c90f1618a5d54a63fef060b6860b947de547383
Chhitiz Buchasia <25711212+buchasia@users.noreply.github.com>
2020-12-21 19:29:54 +0100

    Finished Day 3 Part 2

--- D03/D03.py
+++ D03/D03.py
@@ -5,21 +5,24 @@ def getInput(inputPath):
     return [list(map(int, list(regex.findall(value.strip())[0])))
             for value in fileP.readlines()]
 
-def solvePart1(inputData):
+def solveParts(inputData):
     fabric = {}
     for dataLine in inputData:
         fabric.update({ (i, j):  '*' if (i, j) in fabric else '#'
                 for j in range(dataLine[2], dataLine[2] + dataLine[4])
                 for i in range(dataLine[1], dataLine[1] + dataLine[3]) })
 
-    return list(fabric.values()).count('*')
-    
-def solvePart2(inputData):
-    pass
+    for dataLine in inputData:
+        nonOverLapId = dataLine[0] if sum([1 if fabric[(i, j)] == '*' else 0
+                               for j in range(dataLine[2], dataLine[2] + dataLine[4])
+                               for i in range(dataLine[1], dataLine[1] + dataLine[3])]) == 0 else -1
 
+        if nonOverLapId != -1:
+            break                  
+    return [list(fabric.values()).count('*'), nonOverLapId]
+    
 def solve(inputPath):
     inputData = getInput(inputPath)
-    print(solvePart1(inputData))
-    print(solvePart2(inputData))
+    print(solveParts(inputData))
             
 solve('D03.txt')

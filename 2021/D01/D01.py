225397cab06c04a36a768bde37a877bffa1d34a9
Chhitiz Buchasia <chhitiz.buchasia@abatplus.de>
2021-12-05 01:12:53 +0100

    Finished Day 1 and Day 2

---
+++ 2021/D01/D01.py
@@ -0,0 +1,52 @@
+import math
+
+def getAllData(inputPath):
+
+    measurements = []
+    
+    # Open file for reading measurements
+    with open(inputPath) as fileP:
+
+        # Read the first measurement
+        valueLine = fileP.readline()
+
+        while valueLine:
+
+            measurement = int(valueLine)
+
+            measurements.append(measurement)
+
+            # Read the next measurement
+            valueLine = fileP.readline()
+
+    return measurements
+            
+
+# This method will compute the measurements that are larger than
+# the previous measurements
+def largerMeasurement(measurements, sumIndex):
+
+    # Totalcount of measurements that are larger than the previous
+    # measurements
+    count = 0
+    
+    # loop over the measurements
+    for i in range(len(measurements) - sumIndex + 1):
+
+        # For first index we just do a tmp
+        if i == 0:
+            tmpMeasurement = sum(measurements[i:i + sumIndex])
+            continue
+
+        if sum(measurements[i:i + sumIndex]) > tmpMeasurement:
+                count += 1
+
+        tmpMeasurement = sum(measurements[i:i + sumIndex])
+        
+    return count
+
+# Part 1
+print('Part 1:', largerMeasurement(getAllData('InputD01.txt'), 1))
+
+# Part 2
+print('Part 2:', largerMeasurement(getAllData('InputD01.txt'), 3))
---
+++ 2021/D02/D02.py
@@ -0,0 +1,56 @@
+import math
+
+def getAllData(inputPath):
+
+    directions = {}
+
+    count = 0
+    
+    # Open file for reading directions
+    with open(inputPath) as fileP:
+
+        valueLine = fileP.readline()
+
+        while valueLine:
+
+            directions[count] = {}
+            
+            [direction, movement] = valueLine.split(' ')
+        
+            if direction == 'forward':
+                directions[count][direction] = int(movement)
+            elif direction == 'down':
+                directions[count]['depth'] = int(movement)
+            elif direction == 'up':
+                directions[count]['depth'] = -1 * int(movement)
+
+            valueLine = fileP.readline()
+
+            count += 1
+            
+    return directions
+            
+
+def getPosition(directions, workWithAim):
+
+    x = 0
+    y = 0
+    a = 0
+    
+    # loop over the directions
+    for steps in directions:
+        for key in directions[steps]:
+            if key == 'forward':
+                x += directions[steps][key]
+                y += directions[steps][key] * workWithAim * a
+            else:
+                y += (1 - workWithAim) * directions[steps][key]
+                a += directions[steps][key]
+        
+    return x * y
+
+# Part 1
+print('Part 1:', getPosition(getAllData('InputD02.txt'), 0))
+
+# Part 2
+print('Part 2:', getPosition(getAllData('InputD02.txt'), 1))
